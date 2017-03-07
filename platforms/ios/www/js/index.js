bbf0b6fd6d9bc76c294d54d819c82c8f65b3aedb
Rob Gaston <rgaston@fargeo.com>
2017-03-07 11:59:46 -0800

    adding pouchdb upsert, querying arches server

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -3,38 +3,41 @@ require([
     'js/require-config.js'
 ], function(cordova) {
     require([
+        'jquery',
         'knockout',
         'onsenui'
-    ], function(ko, onsenui) {
+    ], function($, ko, onsenui) {
         var vm = {
             projects: ko.observableArray()
         };
-        
-        document.addEventListener('deviceready', function () {
-            var db = new PouchDB('palmsprings_projects.db', {adapter: 'cordova-sqlite', iosDatabaseLocation: 'Library'});
-            
-            db.put({
-                _id: 'my-test-project',
-                type: 'project',
-                name: 'my project'
-            }).catch(function (err) {
-                console.log(err);
+
+        document.addEventListener('deviceready', function() {
+            var db = new PouchDB('palmsprings_development.db', {
+                adapter: 'cordova-sqlite',
+                iosDatabaseLocation: 'Library'
             });
             
-            db.put({
-                _id: 'my-test-project-2',
-                type: 'project',
-                name: 'my project 2'
-            }).catch(function (err) {
-                console.log(err);
-            });
-            
-            db.allDocs({'include_docs': true}).then(function (docs) {
-                vm.projects(docs.rows.map(function(row) {
-                    return row.doc;
-                }));
-            }).catch(function (err) {
-                console.log(err);
+            $.get('http://rmg-dev-4.local:8000/project/ccbd1537-ac5e-11e6-84a5-026d961c88e6', function (r) {
+                db.upsert('mock-project', function (doc) {
+                    doc.type = 'project';
+                    doc.name = 'mock project';
+                    doc.graphs = r;
+                }).then(function() {
+                    db.query({
+                        map: function getProjects(doc) {
+                            if (doc.type === 'project') {
+                                emit(doc._id, doc);
+                            }
+                        }.toString()
+                    }).then(function(docs) {
+                        console.log(docs);
+                        vm.projects(docs.rows.map(function(row) {
+                            return row.value;
+                        }));
+                    }).catch(function(err) {
+                        console.log(err);
+                    });
+                });
             });
         }, false);
 

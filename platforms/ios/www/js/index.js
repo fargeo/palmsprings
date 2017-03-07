a98f3e2cadaab58f8878fc21eafabed0b7c39349
Rob Gaston <rgaston@fargeo.com>
2017-03-07 13:55:19 -0800

    wiring up navigation through data lists

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -9,7 +9,9 @@ require([
     ], function($, ko, onsenui) {
         var db;
         var vm = {
+            nav: $("#app-nav")[0],
             projects: ko.observableArray(),
+            selection: ko.observable(),
             startProject: function() {
                 window.location = "form.html";
             },
@@ -22,15 +24,16 @@ require([
                     }.toString()
                 }).then(function(docs) {
                     vm.projects(docs.rows.map(function(row) {
+                        console.log(row.value);
                         return row.value;
                     }));
                 }).catch(function(err) {
                     console.log(err);
                 });
             },
-            updateProjects: function () {
-                $.get('http://localhost:8000/project', function(r) {
-                    r.forEach(function (project) {
+            updateProjects: function() {
+                $.get('http://rmg-dev-4.local:8000/project', function(r) {
+                    r.forEach(function(project) {
                         db.upsert(project.projectid, function(doc) {
                             doc.type = 'project';
                             doc.name = project.name;
@@ -44,16 +47,31 @@ require([
             }
         };
 
+        document.addEventListener('init', function(event) {
+            var page = event.target
+            ko.applyBindings(vm, page);
+        });
+
+        vm.selection.subscribe(function(selection) {
+            var page;
+            if (selection.config && selection.config.resource_models) {
+                page = 'resource-models.html';
+            } else if (selection.widgets && selection.widgets.length > 0) {
+                page = 'widgets.html';
+            } else if (selection.cards) {
+                page = 'cards.html';
+            }
+            vm.nav.pushPage(page);
+        });
+
         document.addEventListener('deviceready', function() {
             db = new PouchDB('palmsprings_development.db', {
                 adapter: 'cordova-sqlite',
                 iosDatabaseLocation: 'Library'
             });
-            
+
             vm.getProjects();
             vm.updateProjects();
         }, false);
-
-        ko.applyBindings(vm);
     });
 });

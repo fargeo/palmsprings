77de6d79fa6da3e3238e8f42f571f49af60e4d89
Rob Gaston <rgaston@fargeo.com>
2017-03-07 12:16:59 -0800

    wiring up request to get project data

--- www/js/index.js
+++ www/js/index.js
@@ -7,41 +7,49 @@ require([
         'knockout',
         'onsenui'
     ], function($, ko, onsenui) {
+        var db;
         var vm = {
             projects: ko.observableArray(),
-            startProject: function(){
+            startProject: function() {
                 window.location = "form.html";
+            },
+            getProjects: function() {
+                db.query({
+                    map: function getProjects(doc) {
+                        if (doc.type === 'project' && doc.graphs) {
+                            emit(doc._id, doc);
+                        }
+                    }.toString()
+                }).then(function(docs) {
+                    vm.projects(docs.rows.map(function(row) {
+                        return row.value;
+                    }));
+                }).catch(function(err) {
+                    console.log(err);
+                });
+            },
+            updateProjects: function () {
+                $.get('http://localhost:8000/project/ccbd1537-ac5e-11e6-84a5-026d961c88e6', function(r) {
+                    db.upsert('mock-project', function(doc) {
+                        doc.type = 'project';
+                        doc.name = 'mock project';
+                        doc.graphs = r;
+                        return doc;
+                    }).then(function() {
+                        vm.getProjects();
+                    });
+                });
             }
         };
 
         document.addEventListener('deviceready', function() {
-            var db = new PouchDB('palmsprings_development.db', {
+            db = new PouchDB('palmsprings_development.db', {
                 adapter: 'cordova-sqlite',
                 iosDatabaseLocation: 'Library'
             });
             
-            $.get('http://rmg-dev-4.local:8000/project/ccbd1537-ac5e-11e6-84a5-026d961c88e6', function (r) {
-                db.upsert('mock-project', function (doc) {
-                    doc.type = 'project';
-                    doc.name = 'mock project';
-                    doc.graphs = r;
-                }).then(function() {
-                    db.query({
-                        map: function getProjects(doc) {
-                            if (doc.type === 'project') {
-                                emit(doc._id, doc);
-                            }
-                        }.toString()
-                    }).then(function(docs) {
-                        console.log(docs);
-                        vm.projects(docs.rows.map(function(row) {
-                            return row.value;
-                        }));
-                    }).catch(function(err) {
-                        console.log(err);
-                    });
-                });
-            });
+            vm.getProjects();
+            vm.updateProjects();
         }, false);
 
         ko.applyBindings(vm);
